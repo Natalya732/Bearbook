f666f56e15a3ec47553c6cfb50b9a99f0c325e26
Natalya732 <nikkupandey0602@gmail.com>
2025-03-24 07:52:44 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./App.tsx";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import { AppProvider } from "@contexts/AppContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
